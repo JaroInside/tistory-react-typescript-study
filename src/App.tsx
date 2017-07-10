ea77cca52ceddf7b163099a4ef93146cb858f003
JaroInside <jaro0116@gmail.com>
2017-07-10 16:38:06 +0900

    add props & state - interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,48 @@ import './App.css';
 
 const logo = require('./logo.svg');
 
-class App extends React.Component<{}, {}> {
+interface AppProps {
+  name: string;
+}
+
+interface AppState {
+  age: number;
+}
+
+class App extends React.Component<AppProps, AppState> {
+
+  constructor(props: {name: string}) {
+    super(props);
+    this.state = {
+      age: 31
+    };
+    this._addAge = this._addAge.bind(this);
+  }
+
   render() {
+
+    const {name} = this.props;
+    const {age} = this.state;
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>Hello {name}</h1>
+        <h1>age : {age}</h1>          
+        <button onClick={this._addAge}>나이 추가</button>
       </div>
     );
   }
+
+  private _addAge(): void {
+    this.setState({
+      age: this.state.age + 1
+    });
+  }
+
 }
 
 export default App;
