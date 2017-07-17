c7275e83161f2adf8b51e99f851de2a1e471b4b5
JaroInside <jaro0116@gmail.com>
2017-07-17 15:49:34 +0900

    add PureComponent

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -6,6 +6,7 @@ interface AppProps {
 
 interface AppState {
   name: string;
+  array: number[];
 }
 
 class App extends React.Component<AppProps, AppState> {
@@ -14,9 +15,11 @@ class App extends React.Component<AppProps, AppState> {
     console.log('App constructor');
     super(props);
     this.state = { 
-      name: 'Jaro'
+      name: 'Jaro',
+      array: [1]
     };
     this.handleChange = this.handleChange.bind(this);
+    this._Click = this._Click.bind(this);
   }
   public handleChange(event: React.FormEvent<HTMLSelectElement>): void {
     this.setState({ name: event.currentTarget.value });
@@ -33,11 +36,12 @@ class App extends React.Component<AppProps, AppState> {
   componentWillReceiveProps(nextProps: AppProps) {
     console.log(`Parent componentWillReceiveProps : ${JSON.stringify(nextProps)}`);
   }
-
+  /*
   shouldComponentUpdate(nextProps: AppProps, nextState: AppState): boolean {
     console.log(`App shouldComponentUpdate : ${JSON.stringify(nextProps)}, ${JSON.stringify(nextState)}`);
     return true;
   }
+  */
 
   componentWillUpdate(nextProps: AppProps, nextState: AppState) {
     console.log(`App componentWillUpdate : ${JSON.stringify(nextProps)}, ${JSON.stringify(nextState)}`);
@@ -54,6 +58,8 @@ class App extends React.Component<AppProps, AppState> {
     return (
       <div className="App">
         <h1>Composition Test</h1>
+        <h1>{this.state.array.join(', ')}</h1>
+        <button onClick={this._Click}>추가</button>
         <HelloForm
           name = {this.state.name}
           handleChange = {this.handleChange} 
@@ -63,6 +69,14 @@ class App extends React.Component<AppProps, AppState> {
         />
       </div>
     );
+  }
+
+  private _Click() {
+    const next: number[] = this.state.array;
+    next.push(this.state.array[this.state.array.length - 1] + 1);
+    this.setState({
+      array: next
+    });
   }
 }
 
