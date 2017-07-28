56becc8309e186dd6f9e436bfd605cc76cfe1ecf
JaroInside <jaro0116@gmail.com>
2017-07-28 12:32:05 +0900

    fix middleware & async reducer

--- src/middleware/index.tsx
+++ src/middleware/index.tsx
@@ -1,25 +1,21 @@
-import { Middleware } from 'redux';
+import { Middleware, MiddlewareAPI, Dispatch, Action } from 'redux';
 
-export const middleware: Middleware = api => next => action => {
+export const middleware: Middleware = 
+(api: MiddlewareAPI<void>) => 
+(next: Dispatch<void>) => 
+<A extends Action>(action: A) => {
   console.log(`첫번째 미들웨어 before Value - ${JSON.stringify(api.getState())}`);
   const returnValue = next(action);
   console.log(`첫번째 미들웨어 after Value - ${JSON.stringify(api.getState())}`);
   return returnValue;
 };
 
-export const middlewareB: Middleware = api => next => action => {
+export const middlewareB: Middleware = 
+(api: MiddlewareAPI<void>) => 
+(next: Dispatch<void>) => 
+<A extends Action>(action: A) => {
   console.log(`두번째 미들웨어 before Value - ${JSON.stringify(api.getState())}`);
   const returnValue = next(action);
   console.log(`두번째 미들웨어 after Value - ${JSON.stringify(api.getState())}`);
   return returnValue;
 };
-
-/*
-export const middleware: Middleware =
-  (api: MiddlewareAPI<void>) => (next: Dispatch<void>) => <A extends Action>(action: A) => {
-    console.log(api.getState());
-    const returnValue = next(action);
-    console.log(api.getState());
-    return returnValue;
-  };
-*/
